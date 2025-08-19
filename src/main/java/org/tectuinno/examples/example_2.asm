5c7e8b5319288c76da3b8514a153612c34bfb878
pablo-gomez-perez <pablojesus34@hotmail.com>
2025-08-19 00:25:53 -0600

    Ejemplos en ensamblador

---
+++ src/main/java/org/tectuinno/examples/example_2.asm
@@ -0,0 +1,40 @@
+ini:
+    lui x7, 0x10000    
+    addi x3,x0,1
+    sw x3,0(x7)    
+    call delay
+    call parp
+    add x3,x3,x3
+    sw x3, 0(x7)
+	call delay2
+    addi x3,x0,4
+    sw x3,0(x7)
+    call delay
+    jal x0,ini
+
+delay:
+    addi x5, x0, 1 
+    lui x6, 0x3000
+
+tec:
+    sub x6, x6, x5         
+    beq x6, x0, salir  
+    jal x0,tec
+
+delay2:
+    addi x5, x0, 1
+    lui x6, 0x2000
+tec2:
+    sub x6, x6, x5          
+    beq x6, x0, salir
+    jal x0,tec2
+
+parp:
+    addi x8,x0,5
+    addi x5,x0,1
+parp2:
+    addi x9,x0,0
+    sw x9,0(x7)
+delay3:
+    lui x2,1000
+salir:
